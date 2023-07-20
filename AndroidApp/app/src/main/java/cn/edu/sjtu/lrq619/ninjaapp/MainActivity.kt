b384397eb270da01045b66a4d1b69a0499035d9c
SherwinYi <90945683+SherwinYi@users.noreply.github.com>
2023-07-20 10:46:48 +0800

    UI updated.

--- AndroidApp/app/src/main/java/cn/edu/sjtu/lrq619/ninjaapp/MainActivity.kt
+++ AndroidApp/app/src/main/java/cn/edu/sjtu/lrq619/ninjaapp/MainActivity.kt
@@ -1,45 +1,21 @@
 package cn.edu.sjtu.lrq619.ninjaapp
 
-import android.app.Activity
-import android.app.Application.ActivityLifecycleCallbacks
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.content.pm.PackageManager
-import android.graphics.SurfaceTexture
-import android.graphics.Typeface
-import android.hardware.camera2.CameraCaptureSession
-import android.hardware.camera2.CameraDevice
-import android.hardware.camera2.CameraManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.os.Handler
-import android.os.HandlerThread
-import android.provider.ContactsContract.Data
-import android.text.TextUtils.replace
-import android.util.TypedValue
-import android.view.Surface
-import android.view.TextureView
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.res.ResourcesCompat
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentContainer
-import androidx.fragment.app.FragmentContainerView
-import cn.edu.sjtu.lrq619.ninjaapp.WebService.createRoom
 import cn.edu.sjtu.lrq619.ninjaapp.WebService.logoutUser
-import cn.edu.sjtu.lrq619.ninjaapp.fragments.ui.CreateRoomFragment
-import cn.edu.sjtu.lrq619.ninjaapp.fragments.ui.LogSignInFragment
-import cn.edu.sjtu.lrq619.ninjaapp.fragments.ui.LoginFragment
-import cn.edu.sjtu.lrq619.ninjaapp.fragments.ui.MainFragment
-import cn.edu.sjtu.lrq619.ninjaapp.fragments.ui.SigninFragment
-import com.unity3d.player.n
-import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
@@ -59,6 +35,13 @@ class MainActivity : AppCompatActivity() {
 //        Data = application as DataStore
         usernameText = findViewById(R.id.MainUsernameText)
         WebService.initWebService(context = applicationContext)
+
+        val logo : ImageView = findViewById(R.id.ninja_logo)
+        val slideAnimation : Animation = AnimationUtils.loadAnimation(this, R.anim.slide_down_animation)
+        val fadeAnimation : Animation = AlphaAnimation(0.0f,1.0f)
+        fadeAnimation.duration = 2000
+        logo.startAnimation(slideAnimation)
+        //logo.startAnimation(fadeAnimation)
     }
     fun get_permission(){
         if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
