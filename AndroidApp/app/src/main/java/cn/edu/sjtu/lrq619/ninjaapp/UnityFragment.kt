a8188b7ecff47aebbb1ec97b4e830370b0e29065
lrq619 <lrq619@sjtu.edu.cn>
2023-07-22 16:56:38 +0800

    [update] change Game start parameters

--- AndroidApp/app/src/main/java/cn/edu/sjtu/lrq619/ninjaapp/UnityFragment.kt
+++ AndroidApp/app/src/main/java/cn/edu/sjtu/lrq619/ninjaapp/UnityFragment.kt
@@ -143,18 +143,16 @@ class UnityFragment : Fragment() {
         val username0 = responseArgs["username0"]
         val username1 = responseArgs["username1"]
         Log.e("Unity","Received GameStart, username0: "+username0+",username1: "+username1)
-        var player = ""
-        var enemy = ""
+        var identity = 0
         if(GameActivity.username == username0){
-            player = username0
-            enemy = username1 as String
+            identity = 0
         }else{
-            player = username1 as String
-            enemy = username0 as String
+            identity = 1
         }
         val args = JSONObject()
-        args.put("player",player)
-        args.put("enemy",enemy)
+        args.put("username0",username0)
+        args.put("username1",username1)
+        args.put("player_id",identity)
         args.put("room_id",GameActivity.room_id)
         UnityPlayer.UnitySendMessage("GameController","GameStart", args.toString())
 
